9340605641609f0f2e6699278061f82124a08998
cefika <cefika99@live.com>
2026-04-18 08:49:15 +0200

    feat: add WASM crypto bridge and frontend build

---
+++ web/src/types.ts
@@ -0,0 +1,30 @@
+export interface KeyPairs {
+  k: string;
+  v: string;
+  K: string;
+  V: string;
+}
+
+export interface SendResult {
+  r: string;
+  R: string;
+  viewTag: string;
+  spendingPubKey: string;
+}
+
+export interface ScanResult {
+  spendingPubKeys: string[];
+  spendingPrivKeys: string[];
+}
+
+export interface FoundAddress {
+  stealthAddress: string;
+  spendingPrivKey: string;
+  spendingPubKey: string;
+}
+
+export interface Toast {
+  id: number;
+  type: "success" | "error" | "info";
+  message: string;
+}
