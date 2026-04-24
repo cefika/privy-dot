310757d9d2657d1d7df4b0f9ffbc37a31441d9cb
cefika <cefika99@live.com>
2026-04-24 08:26:48 +0200

    chore: bump runtime spec_version to 5

--- blockchain/runtime/src/lib.rs
+++ blockchain/runtime/src/lib.rs
@@ -451,7 +451,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: alloc::borrow::Cow::Borrowed("stack-template-runtime"),
 	impl_name: alloc::borrow::Cow::Borrowed("stack-template-runtime"),
 	authoring_version: 1,
-	spec_version: 4,
+	spec_version: 5,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 2,
